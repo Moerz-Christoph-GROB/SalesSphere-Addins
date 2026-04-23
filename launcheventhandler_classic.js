0dd13a2eb4add2006d86fb846fd9490a004d5688
Christoph Mörz <christoph.moerz@grob.de>
2026-04-23 08:02:22 +0200

    Add launch event handlers for composing messages
    Implement event handlers for new message and appointment composition.

---
+++ launcheventhandler_classic.js
@@ -0,0 +1,37 @@
+"use strict";
+
+console.log("processing launcheventhandler.js");
+
+
+function onNewMessageComposeHandler(event) {
+  setSubject(event);
+}
+function onNewAppointmentComposeHandler(event) {
+  setSubject(event);
+}
+function setSubject(event) {
+  Office.context.mailbox.item.subject.setAsync(
+    "Set by an event-based add-in! (classic)",
+    {
+      "asyncContext": event
+    },
+    function (asyncResult) {
+      // Handle success or error.
+      if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+        console.error("Failed to set subject: " + JSON.stringify(asyncResult.error));
+      }
+
+      // Call event.completed() to signal to the Outlook client that the add-in has completed processing the event.
+      asyncResult.asyncContext.completed();
+    });
+}
+
+
+/**
+ * Entry point for event-based activation.
+ * Registers all supported launch event handlers.
+ */
+Office.actions.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
+// Office.actions.associate("onItemChangedHandler", onItemChangedHandler);
+
+console.log("finished launcheventhandler.js");
